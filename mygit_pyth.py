b8d4d2e3c6c4cba487e7539cf468d8c7cb915ad3
saidovj@gmail.com <62691845+Jamshed-Saidov@users.noreply.github.com>
2020-05-11 12:18:43 +0500

    Changed age in code of class Person

--- mygit_pyth.py
+++ mygit_pyth.py
@@ -2,11 +2,12 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
-        
+
     def myfunc(self):
         print("Hello, my name is " + self.name)
 
 x = Person("John", 35)
+x.age = 40
 
 print(x.name)
 print(x.age)
