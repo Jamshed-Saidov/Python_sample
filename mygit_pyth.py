4c473d2b3eb9657bfefc335c77a4ac762d6b7647
Jamshed-Saidov <62691845+Jamshed-Saidov@users.noreply.github.com>
2020-05-11 11:48:30 +0500

    Update mygit_pyth.py

--- mygit_pyth.py
+++ mygit_pyth.py
@@ -2,8 +2,12 @@ class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
+        
+    def myfunc(self):
+        print("Hello, my name is " + self.name)
 
 x = Person("John", 35)
 
 print(x.name)
 print(x.age)
+x.myfunc()
