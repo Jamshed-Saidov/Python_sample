881bde6e9fdb941a25629347fb1bf67926ee7ea1
saidovj@gmail.com <62691845+Jamshed-Saidov@users.noreply.github.com>
2020-05-11 11:42:50 +0500

    Corrected code of class Person

--- mygit_pyth.py
+++ mygit_pyth.py
@@ -1,10 +1,9 @@
-class Person
+class Person:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
-    def printname(self):
-        print(self.name, self.age)
-
 x = Person("John", 35)
-print(x)
+
+print(x.name)
+print(x.age)
